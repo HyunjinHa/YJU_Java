5659ad754736d7cef89084959f15730189fa8b85
하현진 <121916525+HyunjinHa@users.noreply.github.com>
2023-03-31 17:43:04 +0900

    Delete Test.java

--- Test.java
+++
@@ -1,9 +0,0 @@
-package hello.inheritance;
-
-public class Test {
-    public static void main(String[] args) {
-        // Derived r = new Derived();
-        Derived d = new Derived("생성ㅇㅇㅇ");
-        // Derived 클래스 내에 String 인자를 받는 생성자가 없기 때문에 오류
-    }
-}
