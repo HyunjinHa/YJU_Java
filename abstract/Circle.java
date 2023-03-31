003257eb3a09b470cd53fbaa5c89abe480a74277
하현진 <121916525+HyunjinHa@users.noreply.github.com>
2023-03-31 17:47:31 +0900

    Add files via upload

---
+++ abstract/Circle.java
@@ -0,0 +1,8 @@
+package abstractC;
+
+public class Circle extends Shape {
+    private int radius;
+    public void draw() {
+        System.out.println("원 그리기 메소드");
+    }
+}
---
+++ abstract/Triangle.java
@@ -0,0 +1,7 @@
+package abstractC;
+
+public class Triangle extends Shape {
+    public void draw() {
+        System.out.println("삼각형 그리기 메소드");
+    }
+}
