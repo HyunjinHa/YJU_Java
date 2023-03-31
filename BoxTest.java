ac8cc6b7fc77e05bc683f1980e0bc4fc07cc3007
하현진 <121916525+HyunjinHa@users.noreply.github.com>
2023-03-31 17:25:11 +0900

    Delete BoxTest.java

--- BoxTest.java
+++
@@ -1,9 +0,0 @@
-package hello.lab;
-
-public class BoxTest {
-    public static void main(String[] args) {
-        Box b = new Box(20, 20, 20);
-        System.out.println(b.getVolume());
-
-    }
-}
